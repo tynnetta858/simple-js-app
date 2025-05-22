01226e2752f87d3e0f20cbea89540ba910d836f9
tynnetta858 <tynnetta.muhammad858@gmail.com>
2025-05-22 14:26:45 -0500

    updated objects for pokemonList
    added several Pokemon characters as objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 let pokemonList = [
-  {name: 'Pikachu'},
-  {height: 5},
-  {types: ['electric', 'flying']},
+  {
+    name: 'Pikachu', 
+    height: 5,
+    types: ['electric', 'flying'
+  },
+  {
+    name: 'Squirtle',
+    height: 5,
+    types: ['water']
+  },
+  {
+    name: 'Butterfree',
+    height: 11,
+    types: ['bug', 'flying']
+  }
 ];
