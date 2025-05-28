ed05e7223298518e7fa9fed9179f3ca8abb8ec3d
tynnetta858 <tynnetta.muhammad858@gmail.com>
2025-05-28 08:43:36 -0500

    Added for loop
    for loop added to print Pokémon list to DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,16 @@
 let pokemonList = [
-  {
-    name: 'Pikachu', 
-    height: 5,
-    types: ['electric', 'flying'
-  },
-  {
-    name: 'Squirtle',
-    height: 5,
-    types: ['water']
-  },
-  {
-    name: 'Butterfree',
-    height: 11,
-    types: ['bug', 'flying']
-  }
+  {name: 'Pikachu', height: 5, types: ['electric', 'flying']},
+  {name: 'Squirtle', height: 5, types: ['water']},
+  {name: 'Butterfree', height: 11, types: ['bug', 'flying']}
 ];
+//pokemonList is an array of objects, each object represents a pokemon with its name, height, and types.
+//object are in curly braces{}
+//name, height and types are the properties of the object
+//values are located after semicolon for each property
+
+for (let i=0; i < pokemonList.length; i++) {
+  let pokemon = pokemonList[i];
+  document.writeln(`${pokemon.name} (height:${pokemon.height})` + "</p>");
+}
+
+//Name(height:0.7)
