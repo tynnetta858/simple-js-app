53564eec77008c2d13ac0c6d35e0f10b14ceb957
tynnetta858 <tynnetta.muhammad858@gmail.com>
2025-05-28 11:56:02 -0500

    added If statement
    if statement was added to highlight Pokemons that are taller than 10

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,18 @@ let pokemonList = [
 //name, height and types are the properties of the object
 //values are located after semicolon for each property
 
+
+//for loop to print out: Name(height:0.7) in DOM
 for (let i=0; i < pokemonList.length; i++) {
   let pokemon = pokemonList[i];
   document.writeln(`${pokemon.name} (height:${pokemon.height})` + "</p>");
 }
 
-//Name(height:0.7)
+
+//contional loop to print out pokemons that have a height greater than 10
+for (let i=0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 10){
+    document.writeln(`${pokemonList[i].name} is very large` + '</p>');
+  }
+}
+
