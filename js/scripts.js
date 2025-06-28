3483e6c523d0d1f99b508011dbfa006328cba2ac
tynnetta858 <tynnetta.muhammad858@gmail.com>
2025-06-28 15:36:02 -0500

    eventListener added to button
    - button element added and style via CSS
    -eventListener added to button
    - showDetails, addButtonEventListener , and addListItem functions added

--- js/scripts.js
+++ js/scripts.js
@@ -9,27 +9,64 @@ let pokemonRepository = (function() {
     return pokemonList;
   }
 
+  // function added to IIFE with parameter pokemon
+  function addListItem(pokemon){
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listPokemonItem = document.createElement('li');
+    // button is inside each li element
+    let button = document.createElement('button');
+
+    // pokemon.name used to get the names of each pokemon
+    button.innerText = pokemon.name; 
+    // added a class to button with classList to style button in CSS file
+    button.classList.add('style-button');
+
+      // call the eventListener function after button is appended to DOM
+    addButtonEventListener(button, pokemon);
+
+    // added button to li list with appendChild
+    listPokemonItem.appendChild(button);
+    // added li to ul (main element) with appendChild
+    pokemonList.appendChild(listPokemonItem);
+  }
+
+    // when user clicks on pokemon name, showDetails function will show details about that particular pokemon
+  function addButtonEventListener(button, pokemon) {
+    button.addEventListener('click', function() {
+      showDetails(pokemon);
+    });
+  }
+
+  function showDetails(pokemon){
+    console.log(`pokemon: ${pokemon.name}`);
+    console.log(`height: ${pokemon.height}`);
+    console.log(`types: ${pokemon.types.join(', ')}`);
+  }
+
   function add(item) {
     pokemonList.push(item);
   }
 
+  // call functions with return
   return {
+    add: add,
     getAll: getAll,
-    add: add
+    addListItem: addListItem
   };
 })();
 
-pokemonRepository.getAll().forEach(function(pokemon) {
-  document.writeln(`<p>${pokemon.name} (height:${pokemon.height})</p>`);
-  if (pokemon.height > 10) {
-    document.writeln(`<p>${pokemon.name} is very large!</p>`);
-  }
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+  // forEach loop function will run addListItem function over and over again
 });
 
 
 
+ 
+  
 
-
+ 
 
 
 
